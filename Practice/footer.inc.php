acf37093509b3842cf12488ac9deda213393c3e7
Romikt <117836973+Romikt@users.noreply.github.com>
2022-11-09 17:48:25 +0300

    Add files via upload

---
+++ Practice/footer.inc.php
@@ -0,0 +1,3 @@
+<div class="footer">
+            <h2>Основы PHP изучены!</h2>
+        </div>
---
+++ Practice/logo.inc.php
@@ -0,0 +1,3 @@
+<div class="logo"> 
+<img src="img/1.jpg" alt="изображение">
+</div>                                   
